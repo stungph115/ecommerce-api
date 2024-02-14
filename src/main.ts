ab626418ac6ce9e6081dc1c767ca3d5ee90ca6b9
stungph115 <phamsontung.05111995@gmail.com>
2024-02-14 15:41:06 +0100

    config main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser'
+import * as bodyParser from 'body-parser'
+import { env } from 'env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+    app.enableCors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+    app.use(cookieParser())
+    app.use(bodyParser.json({ limit: '50mb' }))
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+    await app.listen(env.BACKEND_PORT)
 }
+
 bootstrap();
